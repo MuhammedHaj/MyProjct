e13e69d21e0587b84f200f147cf47b23249d107c
DESKTOP-KGB903C\USER <km303991@gmail.com>
2022-11-09 16:36:03 +0200

    no message

--- Assignments/SixthAssignmentPart2.cpp
+++ Assignments/SixthAssignmentPart2.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
 using namespace std;
-void Reverse(int arr[],int count)
+void Reverse(int *arr,int count)
 {
     int list[count];
     for(int i=count-1;i>=0;i--)
     {
         static int j=0;
-        list[j]=arr[i];
+        list[j]=*(arr+i);
         j++;
     }
     for(int i=0 ;i<count;i++)
     {
-        arr[i]=list[i];
+        *(arr+i)=list[i];
     }
     for(int i=0 ;i<count;i++)
     {
-        cout<<list[i]<<endl;
+        cout<<*(arr+i)<<endl;
     }
 }
 int main()
